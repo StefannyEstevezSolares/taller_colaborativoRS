3cf7becec09be05cb1b2a6eeaa5f65a64cf8a4a3
Randolh Estuardo <estuardotecun@gmail.com>
2026-04-25 16:30:40 -0600

    feat: ✨ add inventory total value calculation
    - Uses a generator expression to efficiently calculate the subtotal (price * quantity) for each product.

--- funciones.py
+++ funciones.py
@@ -13,4 +13,11 @@ def escribir_json(archivito, contenido):
     with open(archivito, "w")as archivo:
         guardar = dumps(contenido, indent=4)
         archivo.write(guardar)
-        
+
+
+def valor_total_inventario():
+    productos = leer_json(archivo_productos)
+
+    total = sum(datos["Precio"] * datos["Cantidad"] for datos in productos.values())
+
+    print(f"El valor total del inventario es: Q {total:,.2f}")
