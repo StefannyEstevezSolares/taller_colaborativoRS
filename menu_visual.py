eb04ebf6daddc546fd0e482c4024f3efac2686b3
StefannyEstevezSolares <rlstephie@gmail.com>
2026-04-25 07:27:28 -0600

    feat: :sparkles: added visual menu
    Added the visual menu that will appear on user´s interface

---
+++ menu_visual.py
@@ -0,0 +1,13 @@
+def menu_visual():
+    print("""----------SISTEMA DE INVENTARIO--------------
+          
+          Elija una de las opciones del menú:
+          
+          1. Agregar Productos
+          2. Enlistar Productos
+          3. Actualizar Cantidad
+          4. Eliminar Productos
+          5. Calcular inventario total
+          6. Salir 
+          
+          -------------------------------------------------""")
